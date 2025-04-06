c99d087c40cef5a5e626f45ca1111f0154149244
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-04-06 16:44:06 +0700

    all is ready

--- main.py
+++ main.py
@@ -42,3 +42,64 @@ for i in range(1, num):
     if i == sum_div(i):
         perf +=1
 print(perf)
+
+#6th
+q = int(input())
+for i in range(1, q+1):
+    print(' '*(q-i), '*'*i)
+
+#7th
+s = input('Введите число:')
+while s.isdigit() == False:
+    s = input('Ошибка. Попробуйте еще раз. Введите число:')
+else:
+    print(f'Введено целое число: {s}')
+
+#8th
+
+for i in range(1, 10):
+    one = list(range(1,i+1))
+    two = list(range(9, 9-i, -1))
+    print(*one,sep='', end='')
+    print(f'*8+{i}=', end='')
+    print(*two,sep='')
+print()
+for i in range(1, 10):
+    one = list(range(1,i+1))
+    two = '1'*i
+    print(*one,sep='', end='')
+    print(f'*9+{i+1}=', end='')
+    print(two,sep='')
+print()
+for i in range(1, 10):
+    one = '1'*i
+    two = '1'*i
+    print(one,sep='', end='')
+    print(f'*{two}=', end='')
+    print(int(one)*int(two),sep='')
+
+#9th
+q = int(input())
+def sum_div(num):
+    div = []
+    for i in range(1, num):
+        if num%i == 0:
+            div.append(i)
+    return sum(div)
+for i in range(1, q+1):
+    if sum_div(i) == 1:
+        print(i)
+
+#10th
+num = int(input())
+def sum_div(num):
+    div = []
+    for i in range(1, num):
+        if num%i == 0:
+            div.append(i)
+    return sum(div)
+
+perf = 0
+for i in range(1, num):
+    if i == sum_div(i):
+        print(i)
